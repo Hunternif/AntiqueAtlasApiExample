bfe42a8f8b55616ed80294559caea05a06d66f95
Hunternif <hunternif@gmail.com>
2014-02-09 19:15:10 +0400

    Set up launch configs, mod id and dependencies

---
+++ src/hunternif/mc/atlas/apiex/AtlasApiExampleMod.java
@@ -0,0 +1,12 @@
+package hunternif.mc.atlas.apiex;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid=AtlasApiExampleMod.ID, name=AtlasApiExampleMod.NAME, version=AtlasApiExampleMod.VERSION,
+dependencies="required-after:antiqueatlas@[2.3,)")
+public class AtlasApiExampleMod {
+	public static final String ID = "antiqueatlasapiexample";
+	public static final String NAME = "Antique Atlas API Example";
+	public static final String VERSION = "@@MOD_VERSION@@";
+	public static final String CHANNEL = ID;
+}
