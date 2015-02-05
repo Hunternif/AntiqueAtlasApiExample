7be5a2f005d093071488600eb0743d0a458e0de5
Hunternif <hunternif@gmail.com>
2015-02-05 23:59:08 +0400

    Updated to ForgeGradle and AntiqueAtlas 4.1.1.

--- src/main/java/hunternif/mc/atlas/apiex/AtlasApiExampleMod.java
+++ src/main/java/hunternif/mc/atlas/apiex/AtlasApiExampleMod.java
@@ -11,7 +11,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 public class AtlasApiExampleMod {
 	public static final String ID = "antiqueatlasapiexample";
 	public static final String NAME = "Antique Atlas API Example";
-	public static final String VERSION = "@@MOD_VERSION@@";
+	public static final String VERSION = "2.0";
 	
 	public static final String antiqueAtlasModID = "antiqueatlas";
 	
--- src/main/java/hunternif/mc/atlas/apiex/CustomTilePlacer.java
+++ src/main/java/hunternif/mc/atlas/apiex/CustomTilePlacer.java
@@ -1,10 +1,12 @@
 package hunternif.mc.atlas.apiex;
 
 import hunternif.mc.atlas.api.AtlasAPI;
+import hunternif.mc.atlas.api.TileAPI;
+import hunternif.mc.atlas.client.TextureSet;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * Whenever the player right-clicks a block, a custom tile will be put in the
@@ -14,16 +16,18 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
 public class CustomTilePlacer {
 	public static final String customTileName = "testCustomTile";
 	
-	@ForgeSubscribe
+	@SubscribeEvent
 	public void onPlayerRightClickBlock(PlayerInteractEvent event) {
 		if (!event.entity.worldObj.isRemote && event.action == Action.RIGHT_CLICK_BLOCK) {
-			AtlasAPI.getTileAPI().putCustomTile(event.entity.worldObj, event.entity.dimension,
+			AtlasAPI.getTileAPI().putCustomGlobalTile(event.entity.worldObj,
 					customTileName, event.x >> 4, event.z >> 4);
 		}
 	}
 	
 	public static void registerTextures() {
-		AtlasAPI.getTileAPI().setTextureIfNone(customTileName,
+		TileAPI api = AtlasAPI.getTileAPI();
+		TextureSet textureSet = api.registerTextureSet("My test texture set",
 				new ResourceLocation(AtlasApiExampleMod.ID, "textures/test.png"));
+		api.setCustomTileTexture(customTileName, textureSet);
 	}
 }
